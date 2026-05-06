de1dddc3ad9256161ee967134b7f151d2361496d
Laura <misakyanlaura@gmail.com>
2026-05-06 18:10:02 +0400

    update

--- js/index.js
+++ js/index.js
@@ -1,17 +1,30 @@
 const burger = document.querySelector(".header-burger");
 const mobileNav = document.querySelector(".header-mobile-nav");
 const close=document.querySelector(".close-menu");
+const navlinks=document.querySelectorAll(".header-mobile-nav a");
 
 burger.addEventListener("click", () => {
   mobileNav.classList.toggle("active");
+   document.body.style.overflow='hidden';
 });
 
 document.addEventListener("click", (e) => {
   if (!mobileNav.contains(e.target) && !burger.contains(e.target)) {
     mobileNav.classList.remove("active");
+    document.body.style.overflow='';
+    
   }
 });
 
 close.addEventListener("click",()=>{
 mobileNav.classList.remove("active")
+ document.body.style.overflow='';
+});
+
+navlinks.forEach(link => {
+  link.addEventListener("click",()=>{
+    mobileNav.classList.remove("active");
+    document.body.style.overflow='';
+ })
+  
 });
